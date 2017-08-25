20ef3e85a46e9dad479a2fdcef6a6f33afe18cfa
Peter Will <peter.will@capitalone.com>
2017-08-24 20:45:47 -0400

    moved liklihood functions to new module

--- chapter_5/ml/costFunction.py
+++ chapter_5/ml/costFunction.py
@@ -23,22 +23,11 @@ class MSEL1(CostFunction):
     def eval(self, y_pred, y, theta, **kwargs):
         return (1/y_pred.size)*np.linalg.norm(y_pred - y)**2 + self.lambda_reg * np.linalg.norm(theta, ord=1)
 
-
 class MSEL2(CostFunction):
 
     def __init__(self, lambda_reg=.5):
         self.lambda_reg = lambda_reg
 
     def eval(self, y_pred, y, theta, **kwargs):
-        reg_lambda = kwargs.get("l",.5)
-        return (1/y_pred.size)*np.linalg.norm(y_pred - y)**2 + self.lambda_reg*np.linalg.norm(theta, ord=2)
-
-
-class LogLiklihood(CostFunction):
-
-    def __init__(self, lambda_reg=.5):
-        self.lambda_reg = lambda_reg
-
-    def eval(self, y_pred, y, theta, **kwargs):
-        reg_lambda = kwargs.get("l",.5)
+        reg_lambda = kwargs.get("l", .5)
         return (1/y_pred.size)*np.linalg.norm(y_pred - y)**2 + self.lambda_reg*np.linalg.norm(theta, ord=2)
